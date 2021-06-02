31380df40918448ca5de353987ca70b075f14cb4
arnab-khan <79851587+arnab-khan@users.noreply.github.com>
2021-06-02 18:24:45 +0530

    Add files via upload

---
+++ assignments.js
@@ -0,0 +1,10 @@
+function createAccount() {
+    document.getElementsByClassName("create_account")[0].style.display="block";
+    document.getElementsByClassName("form_close")[0].style.display="block";
+    document.getElementsByClassName("black_background")[0].style.display="block";
+}
+function formClose() {
+    document.getElementsByClassName("create_account")[0].style.display="none";
+    document.getElementsByClassName("form_close")[0].style.display="none";
+    document.getElementsByClassName("black_background")[0].style.display="none";
+}
